a85a55629c6e18b8d84d8aca0d7116181805a694
matteoschettini <matteo2002.mm0@gmail.com>
2025-05-09 18:14:43 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import {Button, HStack} from "@chakra-ui/react"
+import {Grid, GridItem, Show} from "@chakra-ui/react"
 
 
 
 function App() {
     return (
-        <HStack>
-            <Button>Click me</Button>
-            <Button>Click me</Button>
-        </HStack>
+            <Grid templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}>
+                <GridItem area={'nav'} bg="coral">Nav</GridItem>
+                <Show above={'lg'}>
+                    <GridItem area={'aside'} bg="gold">Aside</GridItem>
+                </Show>
+                <GridItem area={'main'} bg="dodgerblue">Main</GridItem>
+            </Grid>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Provider>
+      <ChakraProvider>
           <App />
-      </Provider>
+      </ChakraProvider>
   </StrictMode>,
 )
