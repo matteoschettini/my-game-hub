65a75604c4521bdfdd3e7a1a3202634c8dbec278
matteoschettini <matteo2002.mm0@gmail.com>
2025-05-12 16:55:28 +0200

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import useGenres, {Genre} from "@/hooks/useGenres.ts";
-import {Button, HStack, Image, List, ListItem} from "@chakra-ui/react";
+import {Button, Heading, HStack, Image, List, ListItem} from "@chakra-ui/react";
 import GenreListSkeleton from "@/components/GenreListSkeleton.tsx";
 
 interface Props {
@@ -14,6 +14,9 @@ function GenreList({onSelectGenre, selectedGenre}: Props) {
     if (error) return null;
 
     return <>
+        <Heading fontSize={"2xl"}
+                 marginBottom={3}>
+            Genres</Heading>
         <List>
             {isLoading && skeleton.map(skeleton => (
                 <GenreListSkeleton key={skeleton}></GenreListSkeleton>
@@ -21,13 +24,16 @@ function GenreList({onSelectGenre, selectedGenre}: Props) {
             {data.map(genre => (
                 <ListItem key={genre.id} paddingBottom={"14px"}>
                     <HStack>
-                        <Image src={genre.image_background}
+                        <Image objectFit={"cover"}
+                               src={genre.image_background}
                                boxSize={"32px"}
                                borderRadius={"8px"}></Image>
-                        <Button fontWeight={selectedGenre === genre ? "bold" : "normal"}
-                            onClick={() => onSelectGenre(genre)}
-                            variant={"link"}
-                            fontSize={"lg"}>{genre.name}</Button>
+                        <Button textAlign={"left"}
+                                whiteSpace={"normal"}
+                                fontWeight={selectedGenre === genre ? "bold" : "normal"}
+                                onClick={() => onSelectGenre(genre)}
+                                variant={"link"}
+                                fontSize={"lg"}>{genre.name}</Button>
                     </HStack>
                 </ListItem>
             ))}
